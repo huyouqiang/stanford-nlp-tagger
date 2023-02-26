988687c298173aa8299c16331baa9d7509b0ca04
huyouqiang <1451984637@qq.com>
2023-02-26 10:39:17 +0800

    项目上传

--- CONTRIBUTING.md
+++
@@ -1,9 +0,0 @@
-# Contributing
-
-Feel free to contribute to this repository. Any help is welcome.
-
-## License
-
-By contributing to this, you agree that your contributions will be licensed under its [MIT license](LICENSE).
-
-Thanks for contributing!
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+	namespace StanfordTagger;
+	
+	include 'vendor/autoload.php';
+
+	$pos = new \StanfordTagger\POSTagger();
+
+	// 设置英文目录
+	$pos->setModel('/usr/local/var/www/cixing/stanford-postagger-2018-10-16/models/english-bidirectional-distsim.tagger');
+
+	// 设置stanford-postagger目录
+	$pos->setJarArchive('/usr/local/var/www/cixing/stanford-postagger-2018-10-16/stanford-postagger.jar');
+	$pos->setOutputFormat(StanfordTagger::OUTPUT_FORMAT_SLASH_TAGS);
+	$res=$pos->tag('PHP wrapper for the Stanford Natural Language Processing library. Supports POSTagger and CRFClassifier');
+	$resArr=explode(' ', $res);
+
+	print_r($resArr);
+	// print_r($resArr);
+
+	/*
+	Array
+	(
+	    [0] => PHP_NN
+	    [1] => wrapper_NN
+	    [2] => for_IN
+	    [3] => the_DT
+	    [4] => Stanford_NNP
+	    [5] => Natural_NNP
+	    [6] => Language_NNP
+	    [7] => Processing_NNP
+	    [8] => library_NN
+	    [9] => ._.
+	Supports_VBZ
+	    [10] => POSTagger_NN
+	    [11] => and_CC
+	    [12] => CRFClassifier_NN
+	)
+	*/
+
+
+?>
